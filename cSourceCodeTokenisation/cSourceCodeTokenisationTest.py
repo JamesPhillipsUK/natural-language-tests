c5f41caac766eb3d0408329bf8b32b42ab57e616
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2021-03-28 00:49:53 +0000

    Finished documenting tonight's work.

--- cSourceCodeTokenisation/cSourceCodeTokenisationTest.py
+++ cSourceCodeTokenisation/cSourceCodeTokenisationTest.py
@@ -1,9 +1,9 @@
 """
 cSourceCodeTokenisationTest.py
 
-Testing NLTK's word tokenisation on C source code.
+Testing various methods of tokenisation on C source code.
 
-Sample Data used: exampleText.c - a  C17 program.
+Sample Data used: exampleText.c - a C17 program.
 
 Authors
 -------
@@ -18,6 +18,19 @@ from nltk.text import Text # Text class - methods for parsing a text.
 # nltk.download() # Open NLTK package Downloader
 
 def lexer(text):
+  """
+  This method, given a string of language (C, in thic case), will perform natural language lexical analysis on it, and use that to tokenise the string.
+  
+  Parameters
+  ----------
+  text: string
+    The string of language.
+    
+  Returns
+  -------
+  list
+    The list of tokens.
+  """
   # TODO: build lexical analyser
   # SEE: https://github.com/gcc-mirror/gcc/blob/master/libcpp/lex.c
   # SEE: https://gcc.gnu.org/onlinedocs/cppinternals/Lexer.html
@@ -29,12 +42,14 @@ def lexer(text):
 
 def tokeniseText(text, method):
   """
-  This method, given a text string containing words, will attempt to tokenise it using NLTK.
+  This method, given a text string containing words, will attempt to tokenise it using the method provided.
   
   Parameters
   ----------
   text: string
     The text to tokenise.
+  method: string
+    The method by which to tokenise the text (word_native, word_naive_with_spaces, lexical_analysis).
     
   Returns
   -------
@@ -93,6 +108,7 @@ def runTests(sampleData):
   sampleData: string
     The test data to run the tests on.
   """
+  # Run tests using NLTK's native word tokenisation.
   tokenisedItems = tokeniseText(sampleData, "word_native")
   freq = buildFrequencyDistributionData(tokenisedItems)
   freq.plot(50, cumulative=False) # Plot a frequency graph (first 50 words)
@@ -103,6 +119,7 @@ def runTests(sampleData):
   print("Finding \"#include\"")
   dataText.concordance("#include")
 
+  # Run tests using a naive method of tokenisation with spaces.
   tokenisedItems = tokeniseText(sampleData, "word_naive_with_spaces")
   print("Finding concordance data for naively-tokenised C")
   dataText = Text(tokenisedItems)
@@ -111,6 +128,7 @@ def runTests(sampleData):
   print("Finding \"#include\"")
   dataText.concordance("#include")
 
+  # Run tests using custom lexical analysis.
   tokenisedItems = tokeniseText(sampleData, "lexical_analysis")
   print("Finding concordance data for lexical-analysis-tokenised C")
   dataText = Text(tokenisedItems)
