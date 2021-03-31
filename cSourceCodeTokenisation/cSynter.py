f2c4ae70dbe097e9d76ba0ed38ef2e56867df2b8
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2021-03-31 02:28:16 +0100

    Added symbols which exist within C's syntax.  For this project, I (hopefully) won't need to do much more on syntactic analysis, and will be focusing on the lexical aspect.

--- cSourceCodeTokenisation/cSynter.py
+++ cSourceCodeTokenisation/cSynter.py
@@ -4,4 +4,12 @@ class CSynter:
 
   Basic tools for syntactic analysis of the C Programming Language, up to C17 and the proposed standards for C23.
   '''
+  # A list of grammatical symbols used in the C Programming language
+  # TODO: Complete this list.  THere must be symbols I'm missing.
+  grammaticalSymbolList = [
+    "!", "\"", "\\", "%", "^", "&", "*", "(", 
+    ")", "|", "[", "]", "{", "}", "-", "+", 
+    "=", "/", ",", ".", ":", ";", "?", "<", 
+    ">", "'"]
+  
   
