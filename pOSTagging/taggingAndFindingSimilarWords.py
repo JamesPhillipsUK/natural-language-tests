a35fd8b44c65548cb5f0993900b798247c608b0f
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2021-07-13 16:51:12 +0100

    Implemented method for finding and recording similar words in a corpus.  Not thread-safe because it captures stdout.  I need to implement a method level lock.

--- pOSTagging/taggingAndFindingSimilarWords.py
+++ pOSTagging/taggingAndFindingSimilarWords.py
@@ -17,6 +17,8 @@ import urllib # Python Website Crawler
 import time # time library
 import string # String lib - used to remove punctuation
 import multiprocessing # multithreading
+import sys # system library, used to access stdout
+import io # system I/O library, used as above.
 
 # nltk.download() # Open NLTK package Downloader
 
@@ -34,7 +36,7 @@ def pOSTagText(text):
   list
     The list of tagged words.
   """
-  tokens = nltk.tokenize.word_tokenize(text) # Tokenise the text
+  tokens = nltk.word_tokenize(text) # Tokenise the text
   # Strip out punctuation tokens
   taggedWords = nltk.pos_tag(tokens)
   return taggedWords
@@ -43,24 +45,16 @@ def findSimilarWords(corporaString):
   wordSimilarityTuples = []
   corporaStringArr = nltk.word_tokenize(corporaString)
   corpora = nltk.Text(nltk.word_tokenize(corporaString))
-  for word in corporaStringArr:
-    print(word.lower())
-    similarWords = corpora.similar(word.lower())
-    if isinstance(similarWords, str):
-      print(word.lower() + " is similar to: " + corpora.similar(word.lower()))
-      similarWords = corpora.similar(word.lower()) # find words similar to the word
-      wordSimilarityTuples.append((word, similarWords)) # prepare a tuple of the word and the similar ones
-  # add all of those to a file
-  print ("  Setting filepath.")
+
   filepath = "Similar Words in " + corporaString.split("\r")[0].split("Book of ")[1] + time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime()) + "GMT.txt"
-  print ("  Writing file.")
-  filePointer = open(filepath, "w")
-  for data in wordSimilarityTuples:
-    filePointer.write(data[0] + "/" + data[1] + "\n")
-  print ("  Written!")
-  filePointer.close()
-  #
-  pass
+  print("Finding similar words in the corpora.  This may take a while.")
+  from contextlib import redirect_stdout
+  with open(filepath, 'w') as filePointer:
+    with redirect_stdout(filePointer):
+      for word in corporaStringArr:
+        print("-----\n" + word.upper() + " is similar to: ")
+        corpora.similar(word.lower(), 5) # This prints to the screen and doesn't actually return anything.  IKR?!
+  print("Done!  See: " + filepath)
 
 def pullText():
   """
@@ -72,8 +66,8 @@ def pullText():
     A list of all of the data from Project Gutenberg in string format.  We load all this straight into memory to save the overhead of grabbing it later.
   """
   theTimeMachine = urllib.request.urlopen(urllib.request.Request(url='https://www.gutenberg.org/files/35/35-0.txt', headers={'User-Agent': 'Mozilla/5.0'}))
-  lesMis = urllib.request.urlopen(urllib.request.Request(url='https://www.gutenberg.org/files/135/135-0.txt', headers={'User-Agent': 'Mozilla/5.0'}))
-  return [str(theTimeMachine.read().decode('utf8')),str(lesMis.read().decode('utf8'))]
+  #lesMis = urllib.request.urlopen(urllib.request.Request(url='https://www.gutenberg.org/files/135/135-0.txt', headers={'User-Agent': 'Mozilla/5.0'}))
+  return [str(theTimeMachine.read().decode('utf8'))]#,str(lesMis.read().decode('utf8'))
 
 def runTests(sampleData):
   """
